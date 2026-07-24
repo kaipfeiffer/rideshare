9ab605bfc25f76e9651ebd117dea28c4b5eb614c
kai <kp@paecher.com>
2026-07-24 13:33:52 +0200

    added custom message for missing autocomplete settings

--- includes/abstracts/class-form-table-abstract.php
+++ includes/abstracts/class-form-table-abstract.php
@@ -10,4 +10,14 @@ use  \KaiPfeiffer\WPBase\Abstracts\FormTableAbstract;
 
 class Form_Table_Abstract extends FormTableAbstract
 {
+    public function _construct($args = array())
+    {   
+        parent::__construct($args);
+        add_filter('wpbase_no_autocomplete_settings_text', array($this, 'wpbase_no_autocomplete_settings_text_filter'), 10, 1);
+    }
+
+    public function wpbase_no_autocomplete_settings_text_filter($text)
+    {
+        return __('No autocomplete SETTINGS provided. Take a look at the documentation in wpbase/Setup.md.', 'wpbase');
+    }
 }
